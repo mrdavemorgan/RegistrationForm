8eeb276e1f60c08d6db4694c759ba7ab34dd60fe
Dave Morgan <davemor@us.ibm.com>
2016-09-23 08:34:00 -0400

    altering index.php to handle unauthenticated state without redirect

--- source/index.php
+++ source/index.php
@@ -1,12 +1,6 @@
 <?PHP
 require_once("./include/membersite_config.php");
-
-if(!$fgmembersite->CheckLogin())
-{
-    $fgmembersite->RedirectToURL("login.php");
-    exit;
-}
-
+$isAuthenticated = $fgmembersite->CheckLogin();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
@@ -17,13 +11,19 @@ if(!$fgmembersite->CheckLogin())
 </head>
 <body>
 <div id='fg_membersite_content'>
-<h2>Home Page</h2>
-Welcome back <?= $fgmembersite->UserFullName(); ?>!
-
-<p><a href='change-pwd.php'>Change password</a></p>
-<p><a href='invitations.php'>Invitations</a></p>
-<br><br><br>
-<p><a href='logout.php'>Logout</a></p>
+<?PHP if($isAuthenticated) { ?>
+	<h2>Home Page</h2>
+	Welcome back <?= $fgmembersite->UserFullName(); ?>!
+	<p><a href='change-pwd.php'>Change password</a></p>
+	<p><a href='invitations.php'>Invitations</a></p>
+	<br><br><br>
+	<p><a href='logout.php'>Logout</a></p>
+<?PHP } else { ?>
+	<h2>Home Page</h2>
+	Unauthenticated
+	<p><a href='login.php'>Login</a></p>
+	<p><a href='register.php'>Register</a></p>
+<?PHP } ?>
 </div>
 </body>
 </html>
